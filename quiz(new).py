b4356297e18ebc5a7b16047acf0155a49db15901
quequuen <rkdusl2020@gmail.com>
2025-10-19 16:56:20 +0900

    Add: 퀴즈(신규) 풀이 추가

---
+++ quiz(new).py
@@ -0,0 +1,50 @@
+#6, 7, 8, 9, 10, 11, 12
+def hello(n):
+    #6, 7
+    #print("안녕하세요\n"*3)
+    #print()
+    
+    #8
+    #hi = 0
+    #while(hi<3):
+    #    print("안녕하세요")
+    #    hi += 1
+    #print()
+    
+    #9
+    #for n in range(3):
+    #    print("안녕하세요")
+    #print()
+    
+    #10
+    #print("안녕하세요\n"*n, end="\n")
+
+    #11
+    i = 0
+    while(i<n):
+        print("안녕하세요")
+        i += 1
+
+n = int(input("숫자를 입력하세요:"))
+hello(n)
+
+#5
+for n in range(3):
+    print("안녕하세요")
+print()
+
+#4
+i = 0
+while(i<3):
+    print("안녕하세요")
+    i += 1
+print()
+
+#3
+print("안녕하세요\n"*3)
+
+#2
+print("안녕하세요\n안녕하세요\n안녕하세요\n")
+
+#1
+print("안녕하세요")
