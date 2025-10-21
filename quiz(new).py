9734ff483cb6328807035ad938c4ec77d5d6b93a
quequuen <rkdusl2020@gmail.com>
2025-10-21 19:34:55 +0900

    Add: ~24(final)번 풀이

--- quiz(new).py
+++ quiz(new).py
@@ -1,3 +1,108 @@
+#24 개선 버전2
+def add_list2(list1, list2):
+    # zip으로 같은 인덱스끼리 더하기
+    result = [a + b for a, b in zip(list1, list2)]
+    
+    # 남는 부분(더 긴 리스트의 뒷부분) 추가
+    if len(list1) > len(list2):
+        result += list1[len(list2):]
+        # 인덱스는 0부터 시작하기 때문에 list2의 마지막 숫자는 포함하지 않고 그 다음 숫자부터 포함됨.
+    else:
+        result += list2[len(list1):]
+        
+    return result
+
+print(add_list2([1,2,3,4],[1,2]))
+print(add_list2([0,1],[1,2,6,7,8]))
+
+#24 개선 버전1
+def add_list1(list1, list2):
+    # 더 긴 리스트 복사 (원본 변경 방지)
+    result = list1[:] if len(list1) >= len(list2) else list2[:]
+    
+    # 짧은 리스트 길이만큼 반복
+    min_len = min(len(list1), len(list2))
+    
+    for i in range(min_len):
+        result[i] = list1[i] + list2[i]
+    
+    return result
+
+print(add_list1([1,2,3,4],[1,2]))
+print(add_list1([0,1],[1,2,6,7,8]))
+
+#24
+
+def add_list(list1, list2):
+    result_list = (list1 if len(list1) > len(list2) else list2)
+    # 일단 리스트의 범위를 넘는 인덱스를 조회하는 것에서부터 오류가 남.
+
+    for i, n in enumerate(list1 if len(list1) < len(list2) else list2):
+        result_list[i] = result_list[i] + n
+        
+    return result_list
+
+print(add_list([1,2,3,4],[1,2]))
+print(add_list([0,1],[1,2,6,7,8]))
+
+#23
+def search_index(list1, num):
+    list2 = []
+    for i,n in enumerate(list1):
+        if n == num :
+            list2.append(i)
+
+    return list2
+
+print(search_index([35, 28, 30, 29, 30],30))
+print(search_index([35, 28, 30, 29, 30],31))
+
+#22
+
+def merge_list(list1, list2):
+     # 두 리스트 합치기
+    merged = list1 + list2
+
+    # 중복 제거 (직접 검사)
+    unique = []
+    for n in merged:
+        if n not in unique:
+            unique.append(n)
+
+    print(unique)
+
+    # 오름차순 정렬 (버블 정렬)
+    for i in range(len(unique) - 1):
+        for j in range(len(unique) - 1 - i):
+            if unique[j] > unique[j + 1]:
+                unique[j], unique[j + 1] = unique[j + 1], unique[j]
+
+    return unique
+
+print(merge_list([1,2,3,4],[1,2,5]))
+print(merge_list([0,1,10],[1,2,6,7,8]))
+
+#21
+def add_dict(scores, name, score):
+    if name in scores.keys():
+        return False
+    else:
+        scores[name] = score
+        return True
+
+scores = {"kim": 95, "lee": 65}
+if add_dict(scores, "park", 100):
+    print("입력 완료")
+else:
+    print("동일 학생 있음")
+
+if add_dict(scores, "kim", 100):
+    print("입력 완료")
+else:
+    print("동일 학생 있음")
+
+print(scores)
+
 #20
 def get_students(scores):
     return scores.keys() if get_students else []
