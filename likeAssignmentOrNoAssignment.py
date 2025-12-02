c3e21fa5e8d4094767308a2fa05177dc6aa116e8
quequuen <rkdusl2020@gmail.com>
2025-12-03 02:17:51 +0900

    Add: 과제인듯과제아닌 파일 일부 구현(Score 클래스, 생성자, str, total 메소드 구현)

--- likeAssignmentOrNoAssignment.py
+++ likeAssignmentOrNoAssignment.py
@@ -1,1 +1,31 @@
 #과제인듯과제아닌
+
+
+class Score():
+    def __init__(self, mid=0, final=0, attend=0, homework=0):
+        self.mid = mid
+        self.final = final
+        self.attend = attend
+        self.homework = homework
+
+    def __str__(self):
+        print(f"{self.mid} / {self.final} / {self.attend} / {self.homework}")
+
+    def total(self):
+        return self.mid + self.final + self.attend + self.homework
+    
+    #0~35점
+    def set_mid(mid):
+        pass
+    
+    #0~35점
+    def set_final(final):
+        pass
+    
+    #0~15점
+    def set_attend(attend):
+        pass
+    
+    #0~15점
+    def set_homework(homework):
+        pass
